980159dec475a061f775fb866859d05ba8f8f54c
sanyaodare@gmail.com <sanyaodare@gmail.com>
2020-10-08 20:35:02 +0100

    first commit

---
+++ understanding-promises.js
@@ -0,0 +1,27 @@
+
+
+export function raceCondition() {
+  let xhr = new XMLHttpRequest();
+  let statuses = [];
+  xhr.open("GET", "http://localhost:3000/orderStatuses");
+  xhr.onload = () => {
+    statuses = JSON.parse(xhr.responseText);
+  };
+
+  xhr.send();
+
+  let xhr2 = new XMLHttpRequest();
+  xhr2.open("GET", "http://localhost:3000/orders/1");
+  xhr2.onload = () => {
+    const order = JSON.parse(xhr2.responseText);
+    const description = statuses.map(t => {
+      if (t.id === order.orderStatusId) {
+        return t.description;
+      }
+    })[0];
+
+    setText(`Order Status: ${description}`);
+  };
+
+  xhr2.send();
+}
